06c675226ecd6364522a7d9bf14f243e36d10f35
meashu291 <meashu29@gmail.com>
2018-02-01 00:27:14 +0530

    changes

--- javacode/app/src/main/java/com/androidcodehub/androidpagingsample/api/ProductResponse.java
+++ javacode/app/src/main/java/com/androidcodehub/androidpagingsample/api/ProductResponse.java
@@ -9,8 +9,5 @@ import java.util.List;
 
 public class ProductResponse {
     public List<Product> products;
-    public int totalProducts;
-    public int pageNumber;
-    public int pageSize;
-    public int status;
+
 }
